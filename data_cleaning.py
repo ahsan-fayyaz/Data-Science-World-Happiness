46ee1e9ae14dc334962c02ff76a7def6be1b60e6
ahsan-fayyaz <ahsan@iknox.com>
2021-11-24 12:45:53 -0500

    more functions

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,7 +7,7 @@ import os
 #Store the datasets into a dictionary
 raw_datasets = {}
 
-#--------------IMPORT ALL THE DATASETS------------#
+###--------------IMPORT ALL THE DATASETS------------###
 raw_world_happiness_df = pd.read_excel("./Datasets/Raw_Datasets/raw_world_happiness.xls", header=0)
 raw_datasets["World Happiness Dataset"] = raw_world_happiness_df
 
@@ -20,23 +20,31 @@ raw_datasets["Drinking Water Services Dataset"] = raw_drinking_water_services_df
 raw_crude_suicide_rates_df = pd.read_csv('./Datasets/Raw_Datasets/raw_crude_suicide_rates.csv',header=0)
 raw_datasets["Crude Suicide Rates Dataset"] = raw_crude_suicide_rates_df
 
-raw_life_expectancy_df = pd.read_csv('./Datasets/Raw_Datasets/raw_life_expectancy_at_birth.csv',header=0)
-raw_datasets["Life Expectancy Dataset"] = raw_life_expectancy_df
+raw_medical_doctors_df = pd.read_csv('./Datasets/Raw_Datasets/raw_medical_doctors.csv',header=0)
+raw_datasets["Medical Doctors Dataset"] = raw_medical_doctors_df
 
 
-#--------------HELPER FUNCTIONS------------#
-# Drop duplicates
+###--------------HELPER FUNCTIONS------------###
+
+#-------clear the Clean_datasets_directory-------#
+def clear_cleaned_dataset_dir():
+    dir = './Datasets/Cleaned_Datasets/'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+        
+        
+#-------Drop duplicates-------#
 def drop_duplicates_from_all_datasets(datasets):
     for name, df in datasets.items():
         df = df.drop_duplicates(inplace=True)
-        print("Duplicates dropped from ", name)
+        print("Duplicates removed from {} dataset", name)
 
 
 
 
-#--------------DATA CLEANING FUNCTIONS FOR EACH DATASET------------#
+###--------------DATA CLEANING FUNCTIONS FOR EACH DATASET------------###
 
-#World Happiness Data
+#-------World Happiness Data-------#
 def clean_world_happiness_data(df):
     #drop unwanted columns
     df = df.drop(['Positive affect', 'Negative affect', 'year'], axis=1)
@@ -52,20 +60,27 @@ def clean_world_happiness_data(df):
                         'Generosity':'generosity',
                         'Perceptions of corruption': 'perceptions_of_corruption'},
                         )
-
     #group by country name
     df = df.groupby(['country'], as_index=False).mean()
     #drop null rows
     df = df.dropna()
     df = clean_world_happiness_data(raw_world_happiness_df)
-    df.to_csv('./Datasets/Cleaned_Datasets/cleaned_world_happiness.csv')
+    # df.to_csv('./Datasets/Cleaned_Datasets/cleaned_world_happiness.csv')
 
 #World Happiness Data
 
 
+
+
+
+
+
+###-----------MAIN-----------###
 def main():
+    clear_cleaned_dataset_dir()
     drop_duplicates_from_all_datasets(raw_datasets)
     clean_world_happiness_data(raw_world_happiness_df)
+    
 
 if __name__ == "__main__":
     main()
