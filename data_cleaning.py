ae4c7cbffed5dc4854fed6b9cf37397a542dc130
ahsan-fayyaz <ahsan@iknox.com>
2021-11-24 11:15:15 -0500

    python script + dir struct

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,37 +1,63 @@
 import pandas as pd
 import os
 
+#--------------GLOBAL VARIABLES------------#
+
+
 #Store the datasets into a dictionary
 raw_datasets = {}
 
+#--------------IMPORT ALL THE DATASETS------------#
+world_happiness_df = pd.read_excel("./Datasets/Raw_Datasets/world_happiness/raw_world_happiness_data.xls", header=0)
+raw_datasets["World Happiness Dataset"] = world_happiness_df
 
-def load_dataframes():
-    #--------------IMPORT ALL THE DATASETS------------#
-    world_happiness_df = pd.read_excel("./Datasets/Raw_Datasets/world_happiness/DataPanelWHR2021C2_1.xls", header=0)
-    raw_datasets["World Happiness Dataset"] = world_happiness_df
+covid_df = pd.read_csv('./Datasets/Raw_Datasets/covid/country_wise_latest.csv',header=0)
+raw_datasets["Covid Dataset"] = covid_df
 
-    covid_df = pd.read_csv('./Datasets/Raw_Datasets/covid/country_wise_latest.csv',header=0)
-    raw_datasets["Covid Dataset"] = covid_df
+drinking_water_services_df = pd.read_csv('./Datasets/Raw_Datasets/other_metrics/basicDrinkingWaterServices.csv',header=0)
+raw_datasets["Drinking Water Services Dataset"] = drinking_water_services_df
 
-    drinking_water_services_df = pd.read_csv('./Datasets/Raw_Datasets/other_metrics/basicDrinkingWaterServices.csv',header=0)
-    raw_datasets["Drinking Water Services Dataset"] = drinking_water_services_df
+crude_suicide_rates_df = pd.read_csv('./Datasets/Raw_Datasets/other_metrics/crudeSuicideRates.csv',header=0)
+raw_datasets["Crude Suicide Rates Dataset"] = crude_suicide_rates_df
 
-    crude_suicide_rates_df = pd.read_csv('./Datasets/Raw_Datasets/other_metrics/crudeSuicideRates.csv',header=0)
-    raw_datasets["Crude Suicide Rates Dataset"] = crude_suicide_rates_df
-
-    life_expectancy_df = pd.read_csv('./Datasets/Raw_Datasets/other_metrics/lifeExpectancyAtBirth.csv',header=0)
-    raw_datasets["Life Expectancy Dataset"] = life_expectancy_df
+life_expectancy_df = pd.read_csv('./Datasets/Raw_Datasets/other_metrics/lifeExpectancyAtBirth.csv',header=0)
+raw_datasets["Life Expectancy Dataset"] = life_expectancy_df
 
 
-
+#--------------HELPER FUNCTIONS------------#
 # Drop duplicates
 def drop_duplicates_from_all_datasets(datasets):
     for name, df in datasets.items():
         df = df.drop_duplicates(inplace=True)
         print("Duplicates dropped from ", name)
 
-# world_happiness_df = world_happiness_df.dropna()
-# world_happiness_df.isnull().sum(axis = 0)
+
+#--------------DATA CLEANING FUNCTIONS FOR EACH DATASET------------#
+
+def clean_world_happiness_data(df):
+    #drop unwanted columns
+    print("Cleaning : World Happiness Data")
+    df = df.drop(['Positive affect', 'Negative affect', 'year'], axis=1)
+    print("Dropped Columns: [Positive affect, Negative affect, year]")
+    print("Renmaed ")
+    df = df.rename(columns={'Country name': 'country',
+                        'Life Ladder': 'life_ladder',
+                        'Log GDP per capita': 'gdp_per_capita',
+                        'Social support': 'social_support',
+                        'Healthy life expectancy at birth': 'life_expectancy',
+                        'oldName1': 'newName1',
+                        'Freedom to make life choices': 'freedom_to_make_life_choices',
+                        'Generosity':'generosity',
+                        'Perceptions of corruption': 'perceptions_of_corruption'},
+                        )
+
+    df = df.groupby(['country'], as_index=False).mean()
+    df = df.dropna()
+    return df
+
+    
+def drop_nulls_from_all_Datasets(df):
+    pass
 
 
 
@@ -50,8 +76,8 @@ def drop_duplicates_from_all_datasets(datasets):
 
 
 def main():
-    load_dataframes()
     drop_duplicates_from_all_datasets(raw_datasets)
+    clean_world_happiness_data(world_happiness_df)
 
 if __name__ == "__main__":
     main()
